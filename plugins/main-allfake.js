161a1b7b5aa5a19640d39308a8cc60bb2029047c
fedelan555 <federicojpg1500@gmail.com>
2025-07-04 22:01:55 -0300

    Update main-allfake.js

--- plugins/main-allfake.js
+++ plugins/main-allfake.js
@@ -97,7 +97,7 @@ global.fake = { contextInfo: { isForwarded: true, forwardedNewsletterMessageInfo
 
 global.icono = [
 'https://files.catbox.moe/do8ghk.jpg',
-'https://files.catbox.moe/7z2ayh.jpg'
+'https://files.catbox.moe/7z2ayh.jpg',
 ].getRandom()
 
 global.rcanal = { contextInfo: { isForwarded: true, forwardedNewsletterMessageInfo: { newsletterJid: channelRD.id, serverMessageId: 100, newsletterName: channelRD.name, }, externalAdReply: { showAdAttribution: true, title: packname, body: dev, mediaUrl: null, description: null, previewType: "PHOTO", thumbnailUrl: icono, sourceUrl: redes, mediaType: 1, renderLargerThumbnail: false }, }, }}
